b15af510428b34aa4c523a5a9e0f7e80d4f8728f
lang.tong <doro2019jp@gmail.com>
2021-08-17 12:00:46 +0000

    3

--- team.rb
+++ team.rb
@@ -14,26 +14,25 @@ class Team
   
   
  def calc_win_rate(win,lose)
-   rate = win.to_f / (win += lose)
-   return rate
+  win.to_f / (win += lose)
  end 
  
-  def show_team_result
- puts "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は#{calc_win_rate(win,lose)}です。"
-  end
+ def show_team_result
+  puts "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は#{calc_win_rate(win,lose)}です。"
+ end
 end
 
 
-giants = Team.new('Giants',67,45,8)
-tigers = Team.new('Tigers',60,53,7)
-dragons = Team.new('Dragons',60,55,5)
-bayStars = Team.new('BayStars',56,58,6)
+giants = Team.new('Giants', 67, 45, 8)
+tigers = Team.new('Tigers', 60, 53, 7)
+dragons = Team.new('Dragons', 60, 55, 5)
+baystars = Team.new('BayStars', 56, 58, 6)
 carp = Team.new('Carp',52,56,12)
-swallows = Team.new('Swallows',41,69,10)
+swallows = Team.new('Swallows', 41, 69, 10)
 
 giants.show_team_result
 tigers.show_team_result
 dragons.show_team_result
-bayStars.show_team_result
+baystars.show_team_result
 carp.show_team_result
 swallows.show_team_result
